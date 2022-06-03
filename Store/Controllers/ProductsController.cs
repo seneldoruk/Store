d0ab6709dc9bf11d88061fcf1be1a8c174d69a25
Doruk <seneldoruk@gmail.com>
2022-06-03 02:17:05 +0200

    init

---
+++ Store/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Store.Data;
+using Store.Entities;
+
+namespace Store.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext _context;
+
+    public ProductsController(StoreContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return Ok( await _context.Products.ToListAsync());
+    }
+
+    [HttpGet("id")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        return Ok(await _context.Products.FindAsync(id));
+        
+    }
+
+}
---
+++ Store/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Store.Data;
+
+namespace Store
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                context.Database.Migrate();
+                DBInitializer.Initialize(context);
+                
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Problem with data migration");
+            }
+            
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
